f0d9eca79e11ea06db9319315496fa899bc9bbda
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-10 11:13:08 +0300

    Add Fontawesome icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../bootstrap_js_files.js'  
+import '../bootstrap_js_files.js'
+import "@fortawesome/fontawesome-free/css/all.css";
 
 Rails.start()
 Turbolinks.start()
