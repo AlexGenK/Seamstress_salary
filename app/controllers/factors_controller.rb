5aaa4332a59fe434a0f0a1ead35548250c5599c7
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-10 19:23:11 +0300

    New factors can be created

--- app/controllers/factors_controller.rb
+++ app/controllers/factors_controller.rb
@@ -1,13 +1,36 @@
 class FactorsController < ApplicationController
-	 before_action :set_factor, only: [:edit, :update, :destroy]
+   before_action :set_factor, only: [:edit, :update, :destroy]
 
-	 def index
-	 	@factors = Factor.order(:min)
-	 end
+   def index
+    @factors = Factor.order(:min)
+   end
 
-	 private
+   def new
+    @factor = Factor.new
+   end
 
-	 def set_factor
-	 	@factor = Factor.find(params[:id])
-	 end
+   def create
+    value_multiplication    
+    @factor = Factor.new(factor_params)
+      if @factor.save
+        redirect_to factors_path
+      else
+        flash[:alert] = 'Невозможно добавить коэффициент'
+        render :new
+      end
+   end
+
+   private
+
+   def value_multiplication
+    params['factor']['value'] = (params['factor']['value'].to_f * 100).to_s
+   end
+
+   def factor_params
+    params.require(:factor).permit(:min, :max, :value)
+   end
+
+   def set_factor
+    @factor = Factor.find(params[:id])
+   end
 end
