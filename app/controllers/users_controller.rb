bba75b3e760340b42bb72384068bb238d45fc6e5
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-11 09:48:51 +0300

    Users can be edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update, :destroy, :edit]
+
   def index
     @users = User.order(:email)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      flash[:alert] = 'Невозможно отредактировать пользователя'
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin_role, :worker_role)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
