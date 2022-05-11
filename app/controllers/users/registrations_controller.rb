4ccc56b7c9540d5b4f41bb58f4e7f922ecb5f952
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-11 10:55:28 +0300

    Allow sign up only for registered users

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!, :redirect_unless_admin, only: [:new, :create]
+  skip_before_action :require_no_authentication
+
+  private
+
+  def redirect_unless_admin
+    unless user_signed_in?
+      flash[:alert] = "Эта операция доступна только зарегистрированным пользователям"
+      redirect_to root_path
+    end
+  end
+
+  def sign_up(resource_name, resource)
+    true
+  end
+end
