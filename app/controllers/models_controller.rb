f3a6fcb6f0c7e24415a4b27ba9db34aaa4801c3d
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-12 12:52:56 +0300

    Models can be added

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,5 +1,25 @@
 class ModelsController < ApplicationController
-	def index
-		@models = Model.order(:number)
-	end
+  def index
+    @models = Model.order(:number)
+  end
+
+  def new
+    @model = Model.new
+  end
+
+  def create
+    @model = Model.new(model_params)
+    if @model.save
+      redirect_to models_path
+    else
+      flash[:alert] = 'Невозможно добавить модель'
+      render :new
+    end
+  end
+
+  private
+
+  def model_params
+      params.require(:model).permit(:number, :name, :sewing)
+    end
 end
