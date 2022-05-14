708c1d8f04735cf2e4d7394451e5d498671002c1
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-14 11:52:38 +0300

    Operations can be added

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -5,9 +5,28 @@ class OperationsController < ApplicationController
     @operations = @model.operations.all.order(:number)
   end
 
+  def new
+    @operation = @model.operations.new
+  end
+
+  def create
+    @operation = @model.operations.new(operation_params)
+    if @operation.save
+      redirect_to model_operations_path(@model)
+    else
+      flash[:alert] = 'Невозможно добавить операцию'
+      render :new
+    end
+  end
+
   private
 
   def set_model
     @model = Model.find(params[:model_id])
+  end
+
+  def operation_params
+    params.require(:operation).permit(:number, :name, :kind, 
+                                      :category, :time, :cost)
   end
 end
