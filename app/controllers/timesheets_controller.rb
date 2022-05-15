86baba767399186de5eadfd5c56b0599f1bd65ec
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-15 20:30:01 +0300

    Timesheets can be added

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -1,5 +1,18 @@
 class TimesheetsController < ApplicationController
-	def index
-		@timesheets = Timesheet.order(:date)
-	end
+  def index
+    @timesheets = Timesheet.order(date: :desc)
+    @timesheet = Timesheet.new(date: Date.today)
+  end
+
+  def create
+    @timesheet = Timesheet.new(timesheet_params)
+    flash[:alert] = 'Невозможно добавить модель' unless @timesheet.save
+    redirect_to timesheets_path
+  end
+
+  private
+
+  def timesheet_params
+    params.require(:timesheet).permit(:date)
+  end
 end
