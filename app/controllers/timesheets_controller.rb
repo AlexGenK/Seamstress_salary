d8e97e6df96179a277666be2fe7f8cdb0cf94a40
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-15 20:35:41 +0300

    Timesheets can be destroyed

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -1,4 +1,6 @@
 class TimesheetsController < ApplicationController
+  before_action :set_timesheet, only: [:destroy]
+
   def index
     @timesheets = Timesheet.order(date: :desc)
     @timesheet = Timesheet.new(date: Date.today)
@@ -6,7 +8,12 @@ class TimesheetsController < ApplicationController
 
   def create
     @timesheet = Timesheet.new(timesheet_params)
-    flash[:alert] = 'Невозможно добавить модель' unless @timesheet.save
+    flash[:alert] = 'Невозможно добавить табель' unless @timesheet.save
+    redirect_to timesheets_path
+  end
+
+  def destroy
+    flash[:alert] = 'Невозможно удалить табель' unless @timesheet.destroy
     redirect_to timesheets_path
   end
 
@@ -14,5 +21,9 @@ class TimesheetsController < ApplicationController
 
   def timesheet_params
     params.require(:timesheet).permit(:date)
+  end
+
+  def set_timesheet
+    @timesheet = Timesheet.find(params[:id])
   end
 end
