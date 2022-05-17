13552e50ea65b71bb17f86c0243cd09a715f7621
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-17 13:47:32 +0300

    Works can be added

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,10 +2,21 @@ class WorksController < ApplicationController
   before_action :set_production
 
   def index
+    @work = @production.works.new
     @works = @production.works.all
   end
 
+  def create
+    @work = @production.works.new(work_params)
+    flash[:alert] = 'Невозможно добавить модель' unless @work.save
+    redirect_to production_works_path(@production)
+  end
+
   private
+
+  def work_params
+    params.require(:work).permit(:model_id, :sum, :time)
+  end
 
   def set_production
     @production = Production.find(params[:production_id])
