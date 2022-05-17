33380431739a3ff531a1fa69cb87ace3aab6952c
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-17 10:15:00 +0300

    Productions reports can be added

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,4 +1,19 @@
 class ProductionsController < ApplicationController
   def index
+    @production = Production.new(date: Date.today)
+    @productions = Production.order(date: :desc, user_name: :asc)
+    @user_names = User.where({worker_role: true}).order(:name).pluck(:name)
+  end
+
+  def create
+    @production = Production.new(production_params)
+    flash[:alert] = 'Невозможно добавить отчет по выработке' unless @production.save
+    redirect_to productions_path
+  end
+
+  private
+
+  def production_params
+    params.require(:production).permit(:user_name, :date, :sum, :time)
   end
 end
