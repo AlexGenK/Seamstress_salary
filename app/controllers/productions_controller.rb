63d16c234f264b5fe7bd24e22ea6c9fc988eeaf3
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-17 10:25:30 +0300

    Productions reports can be deleted

--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,4 +1,6 @@
 class ProductionsController < ApplicationController
+  before_action :set_production, only: [:destroy]
+
   def index
     @production = Production.new(date: Date.today)
     @productions = Production.order(date: :desc, user_name: :asc)
@@ -11,9 +13,18 @@ class ProductionsController < ApplicationController
     redirect_to productions_path
   end
 
+  def destroy
+    flash[:alert] = 'Невозможно удалить отчет по выработке' unless @production.destroy
+    redirect_to productions_path
+  end
+
   private
 
   def production_params
     params.require(:production).permit(:user_name, :date, :sum, :time)
+  end
+
+  def set_production
+    @production = Production.find(params[:id])
   end
 end
