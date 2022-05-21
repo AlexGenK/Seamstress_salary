ef38dccae96b7fea9c7f71c5fe23abafacea8015
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-21 19:07:41 +0300

    Calculate team column sum in the salary report

--- app/services/create_sreport_table_service.rb
+++ app/services/create_sreport_table_service.rb
@@ -20,6 +20,8 @@ class CreateSreportTableService
     end
     
     table_hash.each do |team, users|
+      team_col_sum = {}
+      total_team_sum = 0
       users.each do |user|
         user.each do |name, works|
           y += 1
@@ -35,11 +37,35 @@ class CreateSreportTableService
             change_all_borders(ws.sheet_data[y][x], 'thin')
             x +=1
             row_sum += sum
+            total_team_sum += sum
+            if team_col_sum[name] == nil
+              team_col_sum[name] = sum
+            else 
+              team_col_sum[name] += sum
+            end
           end
+          p team_col_sum
           ws.add_cell(y, 2, row_sum)
           change_all_borders(ws.sheet_data[y][2], 'thin')
           ws.sheet_data[y][2].change_font_bold(:true)
         end
+      end
+      y += 1
+      x = 3
+      ws.add_cell(y, 0, '')
+      change_all_borders(ws.sheet_data[y][0], 'thin')
+      ws.add_cell(y, 1, 'Всего:')
+      change_all_borders(ws.sheet_data[y][1], 'thin')
+      ws.sheet_data[y][1].change_font_bold(:true)
+      ws.sheet_data[y][1].change_horizontal_alignment('right')
+      ws.add_cell(y, 2, total_team_sum)
+      change_all_borders(ws.sheet_data[y][2], 'thin')
+      ws.sheet_data[y][2].change_font_bold(:true)
+      team_col_sum.each do |name, sum|
+        ws.add_cell(y, x, sum)
+        change_all_borders(ws.sheet_data[y][x], 'thin')
+        ws.sheet_data[y][x].change_font_bold(:true)
+        x += 1
       end   
     end
     wb
