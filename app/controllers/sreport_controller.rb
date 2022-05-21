9e6d1fc8883f2b4b3201df5ea177a9f183a16411
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-21 13:52:38 +0300

    Generate simple table of the salary report

--- app/controllers/sreport_controller.rb
+++ app/controllers/sreport_controller.rb
@@ -7,6 +7,8 @@ class SreportController < ApplicationController
     workers_list = GetProductionsListsQuery.workers(current_productions)
     models_list = GetProductionsListsQuery.models(current_productions)
     table_hash = CreateSreportHashService.call(current_productions, workers_list, models_list)
+    table_xl = CreateSreportTableService.call(table_hash, models_list)
     p table_hash
+    send_data table_xl.stream.string, filename: "salaryreport.xlsx", disposition: 'attachment'
   end
 end
---
+++ app/services/create_sreport_table_service.rb
@@ -0,0 +1,39 @@
+class CreateSreportTableService
+  require 'rubyXL'
+  require 'rubyXL/convenience_methods'
+
+  def self.call(table_hash, models_list)
+    wb = RubyXL::Workbook.new
+    ws = wb[0]
+    x = 1
+    y = 0
+    models_list.each do |model|
+      ws.add_cell(y, x, model)
+      ws.sheet_data[y][x].change_horizontal_alignment('center')
+      change_all_borders(ws.sheet_data[y][x], 'thin')
+      x += 1
+    end
+    
+    table_hash.each do |name, works|
+      y += 1
+      ws.add_cell(y, 0, name)
+      change_all_borders(ws.sheet_data[y][0], 'thin')
+        x = 1
+        works.each do |name, sum|
+          ws.add_cell(y, x, sum.to_i == 0 ? '' : sum.to_i)
+          change_all_borders(ws.sheet_data[y][x], 'thin')
+          x +=1
+        end
+    end
+    wb
+  end
+
+  private
+
+  def self.change_all_borders(cell, style)
+    cell.change_border(:left, style)
+    cell.change_border(:right, style)
+    cell.change_border(:top, style)
+    cell.change_border(:bottom, style)
+  end
+end
