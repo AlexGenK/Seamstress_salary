b36a109cb9401165e01bd75c02c974e5b4cda7fe
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-21 18:49:31 +0300

    Calculate row sum in the salary report

--- app/services/create_sreport_table_service.rb
+++ app/services/create_sreport_table_service.rb
@@ -7,7 +7,10 @@ class CreateSreportTableService
     ws = wb[0]
     ws.add_cell(0, 1, 'бригада')
     change_all_borders(ws.sheet_data[0][1], 'thin')
-    x = 2
+    ws.add_cell(0, 2, 'Итого:')
+    change_all_borders(ws.sheet_data[0][2], 'thin')
+    ws.sheet_data[0][2].change_font_bold(:true)
+    x = 3
     y = 0
     models_list.each do |model|
       ws.add_cell(y, x, model)
@@ -25,14 +28,19 @@ class CreateSreportTableService
           ws.add_cell(y, 1, team)
           ws.sheet_data[y][1].change_horizontal_alignment('right')
           change_all_borders(ws.sheet_data[y][1], 'thin')
-          x = 2
+          x = 3
+          row_sum = 0
           works.each do |name, sum|
             ws.add_cell(y, x, sum == 0 ? '' : sum)
             change_all_borders(ws.sheet_data[y][x], 'thin')
             x +=1
+            row_sum += sum
           end
+          ws.add_cell(y, 2, row_sum)
+          change_all_borders(ws.sheet_data[y][2], 'thin')
+          ws.sheet_data[y][2].change_font_bold(:true)
         end
-      end
+      end   
     end
     wb
   end
