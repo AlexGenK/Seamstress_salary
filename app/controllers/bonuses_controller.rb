13e8d1df1a9990af1d8bb59a352b5c5027ea4f0b
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-23 06:50:03 +0300

    Bonuses can be created and destroyed

--- app/controllers/bonuses_controller.rb
+++ app/controllers/bonuses_controller.rb
@@ -1,5 +1,29 @@
 class BonusesController < ApplicationController
-	def index
-		@bonuses = Bonus.order(:date)
-	end
+  before_action :set_bonus, only: [:destroy]
+
+  def index
+    @bonus = Bonus.new(date: Date.today)
+    @bonuses = Bonus.order(:date)
+  end
+
+  def create
+    @bonus = Bonus.new(bonus_params)
+    flash[:alert] = 'Невозможно добавить расчет премиальных коэффициентов' unless @bonus.save
+    redirect_to bonuses_path
+  end
+
+  def destroy
+    flash[:alert] = 'Невозможно удалить расчет' unless @bonus.destroy
+    redirect_to bonuses_path
+  end
+
+  private
+
+  def set_bonus
+    @bonus = Bonus.find(params[:id])
+  end
+
+  def bonus_params
+    params.require(:bonus).permit(:date, :sum_prod, :sum_ts)
+  end
 end
