fdd5126b900efe308c63b7f43bdfe6b1ce0eab83
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-23 19:19:58 +0300

    Personals can be deleted

--- app/controllers/personals_controller.rb
+++ app/controllers/personals_controller.rb
@@ -1,13 +1,23 @@
 class PersonalsController < ApplicationController
   before_action :set_bonus
+  before_action :set_personal, only: [:destroy]
 
   def index
     @personals = @bonus.personals.order(:user_name)
+  end
+
+  def destroy
+    flash[:alert] = 'Невозможно удалить коэффициент' unless @personal.destroy
+    redirect_to bonus_personals_path(@bonus)
   end
 
   private
 
   def set_bonus
     @bonus = Bonus.find(params[:bonus_id])
+  end
+
+  def set_personal
+    @personal = Personal.find(params[:id])
   end
 end
