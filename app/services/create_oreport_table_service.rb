76aff60ba3b74614578a13a44c929a95e8d4d3e5
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-25 13:32:58 +0300

    Moved excel operations to the separated class

--- app/services/create_oreport_table_service.rb
+++ app/services/create_oreport_table_service.rb
@@ -1,6 +1,4 @@
-class CreateOreportTableService
-  require 'rubyXL'
-  require 'rubyXL/convenience_methods'
+class CreateOreportTableService < ExcelOperation
 
   def self.call(model_hash, models_list, date)
     bonus = Bonus.get_by_my(date)
@@ -17,50 +15,5 @@ class CreateOreportTableService
 
   def self.create_table(model_hash, models_list)
     
-  end
-
-  def self.create_cell_bold(y, x, content)
-    @ws.add_cell(y, x, content)
-    @ws.sheet_data[y][x].change_font_bold(:true)
-  end
-
-  def self.create_cell_bold_right(y, x, content)
-    create_cell_bold(y, x, content)
-    @ws.sheet_data[y][x].change_horizontal_alignment('right')
-  end  
-
-  def self.create_cell_border(y, x, content)
-    @ws.add_cell(y, x, content)
-    @ws.sheet_data[y][x].change_border(:left, 'thin')
-    @ws.sheet_data[y][x].change_border(:right, 'thin')
-    @ws.sheet_data[y][x].change_border(:top, 'thin')
-    @ws.sheet_data[y][x].change_border(:bottom, 'thin')
-  end
-
-  def self.create_cell_border_right(y, x, content)
-    create_cell_border(y, x, content)
-    @ws.sheet_data[y][x].change_horizontal_alignment('right')
-  end
-
-  def self.create_cell_border_center(y, x, content)
-    create_cell_border(y, x, content)
-    @ws.sheet_data[y][x].change_horizontal_alignment('center')
-  end
-
-  def self.create_cell_bold_border(y, x, content)
-    create_cell_border(y, x, content)
-    @ws.sheet_data[y][x].change_font_bold(:true)
-  end
-
-  def self.create_cell_bold_border_right(y, x, content)
-    create_cell_bold_border(y, x, content)
-    @ws.sheet_data[y][x].change_horizontal_alignment('right')
-  end
-
-  def self.change_all_borders(cell, style)
-    cell.change_border(:left, style)
-    cell.change_border(:right, style)
-    cell.change_border(:top, style)
-    cell.change_border(:bottom, style)
   end
 end
---
+++ app/services/excel_operation.rb
@@ -0,0 +1,49 @@
+class ExcelOperation
+  require 'rubyXL'
+  require 'rubyXL/convenience_methods'
+
+  def self.create_cell_bold(y, x, content)
+    @ws.add_cell(y, x, content)
+    @ws.sheet_data[y][x].change_font_bold(:true)
+  end
+
+  def self.create_cell_bold_right(y, x, content)
+    create_cell_bold(y, x, content)
+    @ws.sheet_data[y][x].change_horizontal_alignment('right')
+  end  
+
+  def self.create_cell_border(y, x, content)
+    @ws.add_cell(y, x, content)
+    @ws.sheet_data[y][x].change_border(:left, 'thin')
+    @ws.sheet_data[y][x].change_border(:right, 'thin')
+    @ws.sheet_data[y][x].change_border(:top, 'thin')
+    @ws.sheet_data[y][x].change_border(:bottom, 'thin')
+  end
+
+  def self.create_cell_border_right(y, x, content)
+    create_cell_border(y, x, content)
+    @ws.sheet_data[y][x].change_horizontal_alignment('right')
+  end
+
+  def self.create_cell_border_center(y, x, content)
+    create_cell_border(y, x, content)
+    @ws.sheet_data[y][x].change_horizontal_alignment('center')
+  end
+
+  def self.create_cell_bold_border(y, x, content)
+    create_cell_border(y, x, content)
+    @ws.sheet_data[y][x].change_font_bold(:true)
+  end
+
+  def self.create_cell_bold_border_right(y, x, content)
+    create_cell_bold_border(y, x, content)
+    @ws.sheet_data[y][x].change_horizontal_alignment('right')
+  end
+
+  def self.change_all_borders(cell, style)
+    cell.change_border(:left, style)
+    cell.change_border(:right, style)
+    cell.change_border(:top, style)
+    cell.change_border(:bottom, style)
+  end
+end
