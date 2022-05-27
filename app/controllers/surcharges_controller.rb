5e5b95b5fa8ea8326e8af9a75a2aa0f9b752acac
Alexey Kozhyn <suburbakskin@gmail.com>
2022-05-27 18:40:18 +0300

    Surcharges can be added

--- app/controllers/surcharges_controller.rb
+++ app/controllers/surcharges_controller.rb
@@ -1,5 +1,26 @@
 class SurchargesController < ApplicationController
   def index
-    @surcharges = Surcharge.all.order(:date)
+    @surcharges = Surcharge.all.order(:date, :user_name)
+  end
+
+  def new
+    @surcharge = Surcharge.new(date: Date.today)
+    @user_names = User.get_worker_names
+  end
+
+  def create
+    @surcharge = Surcharge.new(surcharge_params)
+    if @surcharge.save
+      redirect_to surcharges_path
+    else
+      flash[:alert] = 'Невозможно добавить начисление'
+      render :new
+    end
+  end
+
+  private
+
+  def surcharge_params
+    params.require(:surcharge).permit(:user_name, :date, :sum, :comment)
   end
 end
