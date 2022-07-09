e2274e6328a303cc10c58f682cd1f95c8293ee2a
Oleksii Kozhhyn <suburbakskin@gmail.com>
2022-07-09 19:43:04 +0300

    Raise errors when worker have zero model cost

--- app/controllers/sreport_controller.rb
+++ app/controllers/sreport_controller.rb
@@ -10,8 +10,14 @@ class SreportController < ApplicationController
     workers_list = GetProductionsListsQuery.workers(current_productions)
     models_list = GetProductionsListsQuery.models(current_productions)
     teams_list = GetProductionsListsQuery.teams(current_productions)
-    sum_hash = CreateSreportHashService.call(current_productions, workers_list, models_list, teams_list, 'sum')
-    time_hash = CreateSreportHashService.call(current_productions, workers_list, models_list, teams_list, 'time')
+    begin
+      sum_hash = CreateSreportHashService.call(current_productions, workers_list, models_list, teams_list, 'sum', flash)
+    rescue => e
+      flash[:alert] = e.message
+      redirect_to edit_sreport_path
+      return
+    end
+    time_hash = CreateSreportHashService.call(current_productions, workers_list, models_list, teams_list, 'time', flash)
     table_xl = CreateSreportTableService.call(sum_hash, time_hash, models_list, report_date)
     send_data table_xl.stream.string, filename: "salaryreport.xlsx", disposition: 'attachment'
   end
--- app/services/create_sreport_hash_service.rb
+++ app/services/create_sreport_hash_service.rb
@@ -1,5 +1,6 @@
 class CreateSreportHashService
-  def self.call(scope, workers_list, models_list, team_list, entity)
+
+  def self.call(scope, workers_list, models_list, team_list, entity, flash)
     a = {} 
     team_list.each do |item|
       a[item] = []
@@ -8,7 +9,7 @@ class CreateSreportHashService
     workers_list.each do |worker|
       oper_hash = {}
       models_list.each do |model|
-        oper_hash[model] = send "get_worker_model_#{entity}", scope, worker, model
+        oper_hash[model] = send "get_worker_model_#{entity}", scope, worker, model, flash
       end
       a[User.find_by(name: worker).team] << {worker => oper_hash}
       table_hash[worker] = oper_hash
@@ -18,12 +19,18 @@ class CreateSreportHashService
 
   private
 
-  def self.get_worker_model_sum(scope, worker, model)
-     worker_model = scope.joins(:works).where('user_name = ? AND works.model_number = ?', worker, model).select('works.sum').first
-     worker_model == nil ? 0 : worker_model.sum.round
+  def self.get_worker_model_sum(scope, worker, model, flash)
+    worker_model = scope.joins(:works).where('user_name = ? AND works.model_number = ?', worker, model).select('works.sum').first
+    if worker_model == nil 
+      return 0
+    elsif worker_model.sum == nil
+      raise "Нулевая строка в отчете по выработке у работника #{worker}"
+    else 
+      return worker_model.sum.round
+    end
   end
 
-  def self.get_worker_model_time(scope, worker, model)
+  def self.get_worker_model_time(scope, worker, model, flash)
      worker_model = scope.joins(:works).where('user_name = ? AND works.model_number = ?', worker, model).select('works.time').first
      worker_model == nil ? 0 : worker_model.time
   end
